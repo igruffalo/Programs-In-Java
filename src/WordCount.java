02be4f5a5264e419072b707b75f31d8f4ec50c71
igruffalo <154533813+igruffalo@users.noreply.github.com>
2026-08-08 21:49:37 -0500

    clean code in count no of words method

--- src/WordCount.java
+++ src/WordCount.java
@@ -4,13 +4,9 @@ import java.util.Map;
 public class WordCount {
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> map = new HashMap<>();
-        String[] words = sentence.split(" ");
+        String[] words = sentence.split("\\W+");
         for (String word : words) {
-            if (map.containsKey(word.toLowerCase())) {
-                map.put(word.toLowerCase(), map.get(word.toLowerCase()) + 1);
-            } else  {
-                map.put(word.toLowerCase(), 1);
-            }
+            map.put(word.toLowerCase(), map.getOrDefault(word.toLowerCase(), 0) + 1);
         }
         return map;
     }
